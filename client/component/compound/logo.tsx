6f1e2e1070b6e4f9bbeec9ad5bdec24cf444a237
Ziphil <yfe23457@nifty.com>
2021-06-17 23:02:12 +0900

    言語選択できるよう変更

--- client/component/compound/logo.tsx
+++ client/component/compound/logo.tsx
@@ -8,16 +8,33 @@ import Component from "/client/component/component";
 import {
   style
 } from "/client/component/decorator";
+import {
+  LANGUAGES
+} from "/client/language";
 
 
 @style(require("./logo.scss"))
 export default class Logo extends Component<Props, State> {
 
   public render(): ReactNode {
+    let languageNodes = LANGUAGES.map((language) => {
+      let languageStyleName = "locale" + ((language.locale === this.props.store!.locale) ? " selected" : "");
+      let languageNode = (
+        <div key={language.locale} styleName={languageStyleName} tabIndex={0} onClick={() => this.props.store!.changeLocale(language.locale)}>{language.name}</div>
+      );
+      return languageNode;
+    });
     let node = (
       <div styleName="root" tabIndex={0} onClick={() => this.pushPath("/")}>
         <img styleName="logo" src="http://ziphil.com/material/logo/2.svg"/>
-        <span styleName="orange">sOxsOt</span> <span styleName="blue">IvO lIvAt</span>
+        <div styleName="text-container">
+          <div styleName="text">
+            <span styleName="orange">sOxsOt</span> <span styleName="blue">IvO lIvAt</span>
+          </div>
+          <div styleName="language">
+            {languageNodes}
+          </div>
+        </div>
       </div>
     );
     return node;
